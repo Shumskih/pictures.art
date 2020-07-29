8c978b9a459bc82b6b3bf6d705e188e99b9419b8
Aleksander Shumeiko <music.dll@mail.ru>
2020-07-29 11:43:05 +0300

    Добавлен модуль, обрабатывающий ввод номера телефона по маске

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,15 @@
+let checkTextInputs = (selector) => {
+    'use strict';
+
+    const txtInputs = document.querySelectorAll(selector);
+
+    txtInputs.forEach(input => {
+        input.addEventListener('keypress', function(e) {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+            }
+        });
+    });
+};
+
+export default checkTextInputs;
