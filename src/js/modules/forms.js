ed7686ea7cd521c5020ca53e00f7ca524a023be5
Aleksander Shumeiko <music.dll@mail.ru>
2020-07-30 04:18:20 +0300

    Метод postData вынесен из forms.js в requests.js

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,4 @@
-// import checkNumInputs from './checkNumInputs';
+import {postData} from '../services/requests';
 
 const forms = () => {
     'use strict';
@@ -21,15 +21,6 @@ const forms = () => {
     const path = {
         designer: 'assets/server.php',
         question: 'assets/question.php'
-    };
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
     };
 
     const clearInputs = () => {
---
+++ src/js/services/requests.js
@@ -0,0 +1,24 @@
+const postData = async (url, data) => {
+    'use strict';
+
+    let res = await fetch(url, {
+        method: "POST",
+        body: data
+    });
+
+    return await res.text();
+};
+
+const getResource = async (url) => {
+    'use strict';
+    
+    let res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+};
+
+export {postData, getResource};
